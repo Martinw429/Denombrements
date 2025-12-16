fc2284369a2e227d4d169e37937b1f9bee4375b3
martin wendy <martinwendy429@gmail.com>
2025-12-16 12:04:21 +0100

    mise du code dans un do while utilisation du switch pour les différent choix du menu

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -8,12 +8,10 @@ namespace Denombrements
         {
             //variable 
             int choix = 1;
-            
-            /// Code permettant de faire des calculs de permutation/arrangement/combinaison sur la console.
-            
-            //Branche teste créer pour optimiser le code et tester différente boucle !
 
-            while (choix != 0)
+            /// Code permettant de faire des calculs de permutation/arrangement/combinaison sur la console.
+
+            do
             {
                 Console.WriteLine("Permutation ...................... 1");
                 Console.WriteLine("Arrangement ...................... 2");
@@ -21,41 +19,35 @@ namespace Denombrements
                 Console.WriteLine("Quitter .......................... 0");
                 Console.Write("Choix :                            ");
                 choix = int.Parse(Console.ReadLine());
-
-                if (choix == 0) { Environment.Exit(0); }
-
-                if (choix == 1)
+                switch (choix)
                 {
-                    Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                    int nombre = int.Parse(Console.ReadLine()); // saisir le nombre
-                                                           // calcul de r
-                    long r = 1;
-                    for (int k = 1; k <= nombre; k++)
-                        r *= k;
-                    Console.WriteLine(nombre + "! = " + r);
-                }
-                else
-                {
-                    if (choix == 2)
-                    {
+                    case 1:
+                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
+                        int nombre = int.Parse(Console.ReadLine()); // saisir le nombre
+                                                                    // calcul de r
+                        long r = 1;
+                        for (int k = 1; k <= nombre; k++)
+                            r *= k;
+                        Console.WriteLine(nombre + "! = " + r);
+                        break;
+                    case 2:
                         Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
                         int t = int.Parse(Console.ReadLine()); // saisir le nombre
                         Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
                         int nombre = int.Parse(Console.ReadLine()); // saisir le nombre
-                        // calcul de r
+                                                                    // calcul de r
                         long r = 1;
                         for (int k = (t - nombre + 1); k <= t; k++)
                             r *= k;
                         //Console.WriteLine("résultat = " + (r1 / r2));
                         Console.WriteLine("A(" + t + "/" + nombre + ") = " + r);
-                    }
-                    else
-                    {
+                        break;
+                    case 3:
                         Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
                         int t = int.Parse(Console.ReadLine()); // saisir le nombre
                         Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
                         int nombre = int.Parse(Console.ReadLine()); // saisir le nombre
-                        // calcul de r1
+                                                                    // calcul de r1
                         long r1 = 1;
                         for (int k = (t - nombre + 1); k <= t; k++)
                             r1 *= k;
@@ -66,9 +58,13 @@ namespace Denombrements
                         // calcul de r3
                         //Console.WriteLine("résultat = " + (r1 / r2));
                         Console.WriteLine("C(" + t + "/" + nombre + ") = " + (r1 / r2));
-                    }
+                        break;
+                    default:
+                        Console.WriteLine("Veuillez saisir un entier");
+                        break;
                 }
-            }
+
+            } while (choix != 0);
             Console.ReadLine();
         }
     }
